bc1b75915968990983af6447456e7ae85848d97f
Md Mazharul Haque <cpon143@gmail.com>
2022-04-12 13:11:37 +0530

    new change

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 let js = "amazing";
 if (js === "amazing")
-    alert("JavaScript is FUN!");
+    // alert("JavaScript is FUN!");
 
 20 + 60 + 80 - 90
 console.log(20 + 60 + 80 - 90);
+console.log("jons");
+console.log(23);
+  
+let firstName="matinda";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// variable namme conventions
+
+let jons_matinda="JM";
+let $function=22;
+
+let person="jons";
+let PI="3.1415";
+
+let myFirstJob="coder";
+let myCurrentJob="teacher";
+
+let job1=("programmer");
+let job2=("teacher");
+
+console.log(myFirstJob);
