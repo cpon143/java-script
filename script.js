83a7d0331afcf7aee2c0b5d1efe9a2098818409a
Md Mazharul Haque <cpon143@gmail.com>
2022-04-14 13:45:45 +0530

    new change

--- script.js
+++ script.js
@@ -28,9 +28,47 @@ let job2=("teacher");
 
 console.log(myFirstJob); */
 
-let javascriptIsFun = true;
+/*let javascriptIsFun = true;
 console.log(javascriptIsFun);
 
 console.log(typeof true);
 console.log(typeof 84);
-console.log(typeof 'kumar');
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year=1991;
+console.log(typeof 1991);
+console.log(1991);*/
+
+/*let age=30;
+age=31;
+// console.log(age);
+const birthYear=2002;
+// birthYear=2000;
+
+var job='programmer';
+programmer='teacher';
+
+lastName='haque';
+console.log(lastName);*/
+
+/*const age=2050;
+const jonsAge=age-2012;
+const bonsAge=age-1987;
+console.log(jonsAge *2, bonsAge/3, 2**4);
+// 2**4 means 2 to the power four 2*2*2*2
+
+const firstName='Jonas';
+const lastName='Lomen';
+console.log(firstName +' '+ lastName);*/
+
+let x = 5 + 10;
+x += 10;
+x *=  4;
+x ++;
+x--;
+x--;
+console.log(x);
