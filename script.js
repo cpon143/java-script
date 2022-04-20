2057402d25136c9993c0910c23b458cad626fe33
Md Mazharul Haque <cpon143@gmail.com>
2022-04-20 16:32:47 +0530

    new change

--- script.js
+++ script.js
@@ -182,10 +182,21 @@ else{
 
 
 const inputYear = '1991';
-console.log(inputYear +18);
-console.log(Number(inputYear) +18);
+console.log(inputYear + 18);
+console.log(Number(inputYear) + 18);
 console.log(typeof inputYear);
 
 console.log(String(1998));
 console.log(typeof NaN);
+console.log(String('23'), 23);
 
+console.log('I am ' + 23 + 'years old');
+console.log('23' - '10' - 3);
+console.log('10' / '6');
+
+let n = '1' + 1; //11
+n = n - 1;
+console.log(n);
+
+m = '10' - '4' - '3' - 2 + '5';
+console.log(m);
