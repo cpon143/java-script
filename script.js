cf5cc2c4c5dccd27698f22284a07a163332be4e8
Md Mazharul Haque <cpon143@gmail.com>
2022-05-16 12:39:09 +0530

    new changes

--- script.js
+++ script.js
@@ -432,6 +432,20 @@ switch (number) {
 // console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
 
 
-const bill = 275;
-const tip = bill<= 300 && bill>= 50 ? 
+/*const bill = 275;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.20;
 
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value 
+${bill + tip}`);*/
+
+/*const bill = 40;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.20;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value 
+${bill + tip}`);*/
+
+const bill = 430;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.20;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value 
+${bill + tip}`);
